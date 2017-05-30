0a7e6ad5a4c55cce9cfbe7cb570bd18213e894f3
Lucas Marques <lucasmarques73@hotmail.com>
2017-05-30 20:12:35 -0300

    Aula sobre tipo de arquivos.

---
+++ tipos.rb
@@ -0,0 +1,31 @@
+#minha_string = String.new('Esta é minha string')
+#minha_string = 'Esta é minha string'
+#
+#puts minha_string.length
+#puts minha_string.reverse
+#puts minha_string.reverse!
+
+#range = (2..4).to_a
+#
+#puts range
+
+#profs = Array.new(3)
+
+#profs[0] = 'L'
+#profs[1] = 'C'
+#profs[2] = 'W'
+
+#profs = Array.[]('L', 'C', 'W')
+#profs2 = Array.[]('F', 'E', 'D')
+#
+#profs.concat(profs2)
+#
+#puts profs
+#puts profs.length
+#puts.clear # Limpa o array
+
+pessoas = Hash['Lucas' => 10, 'Joao' => 20, 'Mateus' => 30]
+
+puts pessoas
+puts "#{pessoas['Lucas']}" #A string deve estar dentro de aspas duplas
+
