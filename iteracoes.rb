bedc571395a8c4dce1f3cf909ca04d6fdec88405
Lucas Marques <lucasmarques73@hotmail.com>
2017-05-30 21:42:39 -0300

    Aula sobre iteration

---
+++ iteracoes.rb
@@ -0,0 +1,48 @@
+
+
+i = 0
+max = 10
+
+#WHILE
+# while i < max do
+#     puts i
+#     i += 1
+# end
+
+#DO WHILE
+# begin
+#     puts i
+#     i += 1
+# end while i < max
+
+#FOR
+# for i in 0..10 do
+#     puts i
+# end
+#  arr = ['Item 1', 'Item 2', 'Item 3']
+# for item in arr do
+#     puts item
+# end
+# for item in (0..arr.length) do
+#     puts arr[item]
+# end
+
+#FOREACH
+# arr = ['Item 1', 'Item 2', 'Item 3']
+
+# arr.each do |a|
+#     puts a
+# end
+
+#UNTIL
+item = 0 
+arr = ['Item 1', 'Item 2', 'Item 3']
+until item > arr.length do
+    puts arr[item]
+    item += 1
+end
+
+
+
+
+
